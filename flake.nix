44f6629c1a070248048af37a0d87c0999ec608f5
Eric Ren <546296+rencire@users.noreply.github.com>
2022-05-18 17:16:22 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,19 +1,13 @@
 {
   description = "A collection of flake templates";
-
   outputs = { self }: {
-
     templates = {
-
-      trivial = {
+      minimal = {
         path = ./minimal;
         description = "A very basic flake with direnv support";
       };
-
     };
-
     defaultTemplate = self.templates.minimal;
-
   };
 }
 
