d8d62a13b2756ad253e263ce37d7a910be3425ec
ren <546296+rencire@users.noreply.github.com>
2022-06-19 00:01:07 -0700

    added rust template

---
+++ rust/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.fenix = {
+    url = "github:nix-community/fenix";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, fenix }:
+    flake-utils.lib.eachDefaultSystem (system: 
+      let
+        pkgs = import nixpkgs {
+          overlays = [ fenix.overlay ];
+          inherit system ;
+        };
+      in 
+      with pkgs; 
+      {
+        devShells.default = mkShell {
+          nativeBuildInputs = [ ];
+          buildInputs = [ 
+            (fenix.packages.${system}.complete.withComponents [
+              "cargo"
+              "clippy"
+              "rust-src"
+              "rustc"
+              "rustfmt"
+            ])
+            rust-analyzer-nightly
+          ];
+        };
+      });
+}
