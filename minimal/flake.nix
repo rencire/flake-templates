07e3e3cb8fe4d6c5fa93060681f3fbc4ee3c3faf
rencire <546296+rencire@users.noreply.github.com>
2023-11-22 20:42:01 +0000

    Update flake.nix

--- minimal/flake.nix
+++ minimal/flake.nix
@@ -6,10 +6,13 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      devShell = pkgs.mkShell {
+    in 
+    with pkgs; {
+      devShell = mkShell {
         nativeBuildInputs = [ pkgs.bashInteractive ];
-        buildInputs = [ ];
+        buildInputs = [ 
+        # add packages here
+        ];
       };
     });
 }
