85ff42f6d7561c2605fddc40caf498166ceff7e7
rencire <546296+rencire@users.noreply.github.com>
2025-07-22 21:22:01 -0700

    refactor: update rust template to use flakelight for cleaner config

--- rust/flake.nix
+++ rust/flake.nix
@@ -1,41 +1,34 @@
 {
   description = "A basic flake with a shell";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flakelight.url = "github:accelbread/flakelight";
   inputs.fenix = {
     url = "github:nix-community/fenix";
     inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
-    {
-      nixpkgs,
-      flake-utils,
-      fenix,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          overlays = [ fenix.overlay ];
-          inherit system;
-        };
-      in
-      with pkgs;
-      {
-        devShells.default = mkShell {
-          nativeBuildInputs = [ ];
-          buildInputs = [
-            (fenix.packages.${system}.complete.withComponents [
-              "cargo"
-              "clippy"
-              "rust-src"
-              "rustc"
-              "rustfmt"
-            ])
-            rust-analyzer-nightly
-          ];
-        };
-      }
-    );
+    { flakelight, fenix, ... }@inputs:
+    flakelight ./. {
+      inherit inputs;
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      withOverlays = [ fenix.overlay ];
+      devShell = pkgs: {
+        packages = with pkgs; [
+          (fenix.packages.${system}.complete.withComponents [
+            "cargo"
+            "clippy"
+            "rust-src"
+            "rustc"
+            "rustfmt"
+          ])
+          rust-analyzer-nightly
+        ];
+      };
+    };
 }
