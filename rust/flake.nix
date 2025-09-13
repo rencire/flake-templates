4b050c54ef81dace629d1c75ece90444c753b3d9
rencire <546296+rencire@users.noreply.github.com>
2025-09-12 21:50:07 -0700

    feat: added crane

--- rust/flake.nix
+++ rust/flake.nix
@@ -1,14 +1,22 @@
 {
-  description = "A basic flake with a shell";
+  description = "A basic flake for rust development";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flakelight.url = "github:accelbread/flakelight";
+  # For nightly rust
   inputs.fenix = {
     url = "github:nix-community/fenix";
     inputs.nixpkgs.follows = "nixpkgs";
   };
+  # For incremental crate builds and caching during development
+  inputs.crane.url = "github:ipetkov/crane";
 
   outputs =
-    { flakelight, fenix, ... }@inputs:
+    {
+      flakelight,
+      fenix,
+      crane,
+      ...
+    }@inputs:
     flakelight ./. {
       inherit inputs;
       systems = [
@@ -17,18 +25,61 @@
         "x86_64-darwin"
         "x86_64-linux"
       ];
-      withOverlays = [ fenix.overlay ];
+      withOverlays = [ fenix.overlays.default ];
+      # Define the package
+      packages = {
+        default =
+          { pkgs, ... }:
+          let
+            rustToolchain = (
+              fenix.packages.${pkgs.system}.minimal.withComponents [
+                "cargo"
+                "rustc"
+              ]
+            );
+            craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+            cargoArtifacts = craneLib.buildDepsOnly {
+              src = ./.;
+              pname = "my-app-deps";
+              version = "0.1.0";
+            };
+          in
+          craneLib.buildPackage {
+            inherit cargoArtifacts;
+            src = ./.;
+            pname = "my-app";
+            version = "0.1.0";
+          };
+      };
+      # Expose as runnable app
+      # Notes: We can grab my-app package from `pkgs` here, because flakelight automatically added it to
+      # overlays.default.  Even for default package, it seems to grab the name from `pname`?
+      apps = {
+        default =
+          { my-app, ... }:
+          {
+            type = "app";
+            program = "${my-app}/bin/my-app";
+          };
+      };
       devShell = pkgs: {
-        packages = with pkgs; [
-          (fenix.packages.${system}.complete.withComponents [
-            "cargo"
-            "clippy"
-            "rust-src"
-            "rustc"
-            "rustfmt"
-          ])
-          rust-analyzer-nightly
-        ];
+        packages =
+          with pkgs;
+          let
+            rustToolchain = (
+              fenix.packages.${pkgs.system}.complete.withComponents [
+                "cargo"
+                "clippy"
+                "rust-src"
+                "rustc"
+                "rustfmt"
+              ]
+            );
+          in
+          [
+            rustToolchain
+            rust-analyzer-nightly
+          ];
       };
     };
 }
